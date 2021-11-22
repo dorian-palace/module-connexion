346a93eee0b9a34834c92bd025281fa4cb7a5786
dorian-palace <dorian.palace@laplateforme.io>
2021-11-22 19:37:02 +0100

    profil

--- profil.php
+++ profil.php
@@ -11,14 +11,58 @@ try{
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
+	if(isset($_SESSION['id'])){
+		$requser = $bdd->prepare("SELECT * FROM utilisateurs WHERE id = ?");
+		$requser->execute(array($_SESSION['id']));
+		$user = $requser->fetch();
+
+		if(isset($_POST['newlogin']) AND !empty($_POST['newlogin']) AND $_POST['newlogin'] != $user['login'])
+		{
+			$newlogin = htmlspecialchars($_POST['newlogin']);
+			$insertlogin = $bdd->prepare("UPDATE utilisateurs SET login = ? WHERE id = ?");
+			$insertlogin->execute(array($newlogin, $_SESSION['id']));
+			header('Location: profil.php?id='.$_SESSION['id']);
+		}
+
+		if(isset($_POST['newprenom']) AND !empty($_POST['newprenom']) AND $_POST['newprenom'] != $user['prenom'])
+		{
+			$newprenom = htmlspecialchars($_POST['newprenom']);
+			$insertprenom = $bdd->prepare("UPDATE utilisateurs SET prenom = ? WHERE id = ?");
+			$insertprenom->execute(array($newprenom, $_SESSION['id']));
+			header('Location: profil.php?id='.$_SESSION['id']);
+		}
+
+		if(isset($_POST['newnom']) AND !empty($_POST['newnom']) AND $_POST['newnom'] != $user['nom'])
+		{
+			$newnom = htmlspecialchars($_POST['newnom']);
+			$insertnom = $bdd->prepare("UPDATE utilisateurs SET nom = ? WHERE id = ?");
+			$insertnom->execute(array($newnom, $_SESSION['id']));
+			header('Location: profil.php?id='.$_SESSION['id']);
+		}
+
+		if(isset($_POST['newpassword1']) AND !empty($_POST['newpassword1']) AND isset($_POST['newpassword2']) AND !empty($_POST['newpassword2'])) {
+			$password1 = sha1($_POST['newpassword1']);
+			$password2 = sha1($_POST['newpassword2']);
+			if($password1 == $password2) {
+			   $insertpassword = $bdd->prepare("UPDATE utilisateurs SET password = ? WHERE id = ?");
+			   $insertpassword->execute(array($password1, $_SESSION['id']));
+			   header('Location: profil.php?id='.$_SESSION['id']);
+			} 
+			
+			else {
+			   $msg = "Vos deux mdp ne correspondent pas !";
+			}
+	}
+
 
 
 }//try
-
-
+}
 catch(PDOException $e){
-    echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
-  }
+    
+    echo 'echec : ' .$e->getMessage();
+}
+
   
 ?>
 	<html>
@@ -28,12 +72,11 @@ catch(PDOException $e){
 	   </head>
 	   <body>
 	      <div align="center">
-              <h2><?php echo $user['prenom']; ?></h2>
 	         <h2>Modifier votre profil</h2>
 	         <br /><br />
 	         <form method="POST" action="">
-	            <input type="text" name="newlogin" placeholder="login" value="<?php echo $user ?>" /><br /><br />
-                <input type="text" name="newprenom" placeholder="prenom" value="" /><br /><br />
+	            <input type="text" name="newlogin" placeholder="login" value="<?php echo $user['login'] ?>" /><br /><br />
+                <input type="text" name="newprenom" placeholder="prenom" value="<?php echo $user['prenom'];  ?>" /><br /><br />
                 <input type="text" name="newnom" placeholder="nom" /><br /><br />
 	            <input type="password" name="newpassword1" placeholder="password" /><br /><br />
                 <input type="password" name="newpassword2" placeholder="password" />
@@ -50,7 +93,3 @@ catch(PDOException $e){
 	      </div>
 	   </body>
 	</html>
-
-<?php
-
-?>
